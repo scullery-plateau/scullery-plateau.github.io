da38711edba0b71fa9b2f4b2ac573b53ea0c4817
Daniel Johnson <dajohnson1978@gmail.com>
2019-02-06 17:36:56 -0600

    building out state machine

--- critroletext/actionhandler.js
+++ critroletext/actionhandler.js
@@ -6,7 +6,14 @@
     },0);
   }
   var resolveTemplate = function(tpl,state) {
-    return eval("`" + tpl.split("${").join("${state.") + "`");
+    if ((tpl + "").indexOf("${") >= 0) {
+      return eval("`" + tpl.split("${").join("${state.") + "`");
+    } else {
+      return tpl;
+    }
+  }
+  var rollCheck = function(bonus,target,success,failure) {
+    return (((roll(20) + bonus) > target) ? success : failure);
   }
   var interaction = {
     "init":{
@@ -23,20 +30,26 @@
     },
     "move":{
       "prompt":["You have chosen to move.","Where would you like to move?"],
-      "input":function(value){
-        map.moveCharacter(ctx.turn,value);
-        return {state:"combat"};
+      "input":function(ui,ctx,value){
+        ui.console.after(function(){
+          map.moveCharacter(ctx.turn,value);
+          ui.output.after(function(){
+            applyToContext(ctx,{state:"combat"});
+            proceed(ui,ctx);
+          })
+        });
+        return {};
       }
     },
     "target":{
       "prompt":["You have chosen to attack.","Choose a foe to attack."],
-      "input":function(value){
+      "input":function(ui,ctx,value){
         return {state:"attack",target:value};
       }
     },
     "attack":{
       "prompt":["You have chosen to attack ${target.name}","Rolling for attack..."],
-      "roll":{check:"attack",bonus:8,target:18,
+      "roll":{bonus:"${self.attack}",target:"${target.armor}",
         success:{state:"hit"},fail:{state:"miss"}}
     },
     "hit":{
@@ -53,14 +66,93 @@
       "prompt":["${turn.name} does ${damage} of damage to ${target.name}"]
     }
   };
+  var applyToContext = function(ctx,update) {
+    Object.entries(update).forEach(function(entry){
+      ctx[entry[0]] = resolveTemplate(entry[1],ctx);
+    });
+  }
+  var resolveUpdate = function(ui,ctx,update,value) {
+    if ((typeof update) == "function") {
+      try {
+        var result = update(ui,ctx,value);
+        applyToContext(ctx,result);
+      } catch e {
+        printTpl(ui,ctx)(e);
+      }
+    } else {
+      applyToContext(ctx,update);
+    }
+  }
+  var steps = {
+    auto:resolveUpdate,
+    roll:function(ui,ctx,roll) {
+      applyToContext(ctx,
+        rollCheck(parseInt(resolveTemplate(roll.bonus,ctx)),
+          parseInt(resolveTemplate(roll.target,ctx)),
+          roll.success,
+          roll.failure));
+    },
+    input:resolveUpdate,
+    opts:function(ui,ctx,opts,opt) {
+      var optNames = Object.keys(opts);
+      if (optNames.indexOf(opt) >= 0) {
+        resolveUpdate(ui,ctx,opts[opt]);
+      } else {
+        [("'" + opt + "' is not a valid response to the prompt."),
+        ("Please type one of '" + optNames.join("','") + "'")].forEach(printTpl(ui,ctx);
+      }
+    }
+  }
+  var printTpl = function(ui,ctx) {
+    return function(line) {
+      ui.console.println(resolveTemplate(line,ctx));
+    }
+  }
+  var proceed = function(ui,ctx) {
+    var currentState = ctx.state;
+    var state = interaction[currentState];
+    if (!state) {
+      throw ("no state exists: " + currentState);
+    }
+    state.prompt.forEach(printTpl(ui,ctx));
+    var event = ["auto","roll"].filter(function(item) {
+      return [item];
+    });
+    if (event.length > 1) {
+      throw ("Invalid construct of state " + ctx.state + ": multiple events - [" + event.join() + "]");
+    } else if (event.length == 1) {
+      event = event[0];
+      var opts = state[event];
+      var step = steps[event];
+      step(ui,ctx,opts);
+      if (currentState != ctx.state) {
+        proceed(ui,ctx);
+      }
+    }
+  };
   window.ActionHandlerFactory = function(config) {
     return function(ui) {
-      var state = {};
+      ui.map = new RogueLikeMap(ui,config);
+      var ctx = {state:"init"};
       this.init = function() {
-        
+        proceed(ui,ctx);
       }
       this.handle = function(action) {
-        
+        var state = interaction[ctx.state];
+        if (!state) {
+          throw ("no state exists: " + ctx.state);
+        }
+        var event = ["opts","input"].filter(function(item) {
+          return [item];
+        });
+        if (event.length != 1) {
+          throw ("Invalid construct of state " + ctx.state + ": multiple events - [" + event.join() + "]");
+        }
+        event = event[0];
+        var opts = state[event];
+        var step = steps[event];
+        step(ui,ctx,opts,action);
+        proceed(ui,ctx);
       }
     }
   }
