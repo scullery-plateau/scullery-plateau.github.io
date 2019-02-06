cb815ca93ff771a87d8a631f5e0794d20e88c061
Daniel Johnson <dajohnson1978@gmail.com>
2019-02-06 09:00:46 -0600

    stubbing out state based action handler

--- critroletext/gamedemo.js
+++ critroletext/gamedemo.js
@@ -11,51 +11,7 @@
           ].forEach(ui.console.println);
         });
       }
-      var interaction = {
-        "init":{
-          "prompt":["Your adventure starts here....","Type 'START' and hit 'ENTER' to begin."],
-          "opts":{"START":{state:"initiative"}}
-        },
-        "initiative":{
-          "prompt":["You are in combat!","It is Vax's turn!"],
-          "auto":{state:"combat",turn:"7"}
-        },
-        "combat":{
-          "prompt":["What do you wish to do?","1 - Move","2 - Attack"],
-          "opts":{"1":{state:"move"},"2":{state:"target"}}
-        },
-        "move":{
-          "prompt":["You have chosen to move.","Where would you like to move?"],
-          "input":function(value){
-            map.moveCharacter(ctx.turn,value);
-            return {state:"combat"};
-          }
-        },
-        "target":{
-          "prompt":["You have chosen to attack.","Choose a foe to attack."],
-          "input":function(value){
-            return {state:"attack",target:value};
-          }
-        },
-        "attack":{
-          "prompt":["You have chosen to attack ${target.name}","Rolling for attack..."],
-          "roll":{check:"attack",bonus:8,target:18,
-            success:{state:"hit"},fail:{state:"miss"}}
-        },
-        "hit":{
-          "prompt":["${turn.name} hits ${target.name}"],
-          "auto":function() {
-            var damage = roll(6,4) + 3;
-            return {state:"damage",damage:damage};
-          }
-        },
-        "miss":{
-          "prompt":["${turn.name} misses ${target.name}."]
-        },
-        "damage":{
-          "prompt":["${turn.name} does ${damage} of damage to ${target.name}"]
-        }
-      };
+      
       var roll = function(side,count){
         if (!count){count = 1;}
         return (side + "").repeat(count).reduce(function(a,b){
@@ -65,7 +21,7 @@
       this.handle = function(action) {
         console.log(action);
         if (action == "START") {
-          var attackRoll = Math.floor(Math.random() * 20) + 1;
+          var attackRoll = roll(20);
           console.log(attackRoll);
           var attackTotal = attackRoll + 8
           var attackLine = attackRoll + " + 8 = " + attackTotal;
