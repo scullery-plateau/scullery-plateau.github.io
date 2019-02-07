1ceb9b3c1a8f10df75e6b2b35d764f96a0ecbaca
scullery-plateau <38331707+scullery-plateau@users.noreply.github.com>
2019-02-07 13:26:50 -0600

    stubbing out mocks and builds to test

--- critroletext/test/tests.js
+++ critroletext/test/tests.js
@@ -1,3 +1,32 @@
 (function(){
-  window.allTests = {};
+  var buildMockUI = function() {
+    var data = [];
+    return {
+        data:data,
+        println:function(str) {
+          data.push(str + "\n);
+        },
+        clearOutput:function(){
+          data.splice(0,data.length);
+        },
+        after:function(fn) {
+          fn();
+        }
+    }
+  }
+  window.allTests = (function(){
+    var buildActionHandler = new ActionHandlerFactory(GameData);
+    var outputData = []
+    var consoleData = [];
+    var mockUI = {
+      output:buildMockUI(),
+      console:buildMockUI()
+    }
+    var actionHandler = buildActionHandler(mockUI);
+    return {
+        "Test 1":function() {
+          assert(false);
+        }
+    };
+  })()
 })();
