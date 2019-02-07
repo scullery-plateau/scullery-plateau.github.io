df0178e3c29824d20329344994c97c39a9866700
scullery-plateau <38331707+scullery-plateau@users.noreply.github.com>
2019-02-07 13:09:47 -0600

    building out simple test runner

---
+++ critroletext/test/testrunner.js
@@ -0,0 +1,26 @@
+(function(){
+  var formatResult = function(result){
+    return '<tr class="' + (result.error?"fail":"success") + '"><td>' + result.name + "</td><td>" + (result.error?result.error:"PASSED") + "</td></tr>";
+  }
+  var runTests = function(testMap,namePath) {
+    return Object.entries(testMap).reduce(function(out,test){
+      var name = namePath + test[0];
+      if ((typeof test[1]) == "function") {
+        try {
+          test[1]();
+          out.push(formatResult({name:name}));
+        } catch(e) {
+          out.push(formatResult({name:name,error:e}))
+        }
+      } else {
+        return out.concat(runTests(test[1],name + "."))
+      }
+      return out;
+    },[]);
+  }
+  window.TestRunner = function(testObj,outputId){
+    this.run = function() {
+      document.getElementById().innerHTML = "<table>" + runTests(testObj,"").join("") + "</table>";
+    }
+  }
+})();
