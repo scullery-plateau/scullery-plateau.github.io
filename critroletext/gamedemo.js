f3dff1e8db38a61f405ec67b6c7da8a7f53044ce
Daniel Johnson <dajohnson1978@gmail.com>
2019-02-23 17:16:48 -0600

    adding error alert for mobile

--- critroletext/gamedemo.js
+++ critroletext/gamedemo.js
@@ -2,7 +2,15 @@
   window.Game = function(outputId,consoleId,config) {
     var cli = new Interface(0,75,outputId,consoleId,ActionHandlerFactory(GameData,GameStates));
     this.init = function() {
-      cli.init();
+      try {
+        cli.init();
+      } catch(e) {
+        if (e instanceof Error) {
+          alert(e.stack);
+        }
+          alert(e);
+        }
+      }
     }
   }
 })()
