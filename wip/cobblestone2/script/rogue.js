585d4180c39c660bdda9a9b21c543f54456098cf
Daniel Johnson <daniel.johnson@surescripts.com>
2019-03-15 16:06:30 -0500

    adjusting ui

--- wip/cobblestone2/script/rogue.js
+++ wip/cobblestone2/script/rogue.js
@@ -10,8 +10,7 @@
     'tilePixelInput',
     'tileDisplay',
     'mapInput',
-    'mapDisplay',
-    'charList',
+    'mapDisplay'
   ];
   var charDomKeys = [
     "collapseLabel",
@@ -23,33 +22,6 @@
     "turnleft",
     "showTile"
   ]
-  var buildCharListItem = function(index){
-    return `<li>
-  <input type="checkbox" checked id="collapseChar${index}" onClick="ctrl.toggleChar(this,${index})"/><label id="collapseLabel${index}" for="collapseChar${index}"></label>
-  <div id="charCtrl${index}">
-    <p><label for="tileForMap${index}">Tile: </label><select id="tileForMap${index}" onselect="ctrl.selectTileForChar(this,${index})"></select></p>
-    <p><label for="paletteForMap${index}">Palette: </label><select id="paletteForMap${index}" onselect="ctrl.selectPaletteForChar(this,${index})"></select></p>
-    <table>
-      <tr>
-        <td>
-          <label>Orient:</label>
-          <ul>
-            <li><input type="checkbox" id="flipdown${index}" onclick="ctrl.applyTransform(this,'flipdown',${index})"/><label for="flipdown${index}">Flip Down</label></li>
-            <li><input type="checkbox" id="flipover${index}" onclick="ctrl.applyTransform(this,'flipover',${index})"/><label for="flipover${index}">Flip Over</label></li>
-            <li><input type="checkbox" id="turnright${index}" onclick="ctrl.applyTransform(this,'turnright',${index})"/><label for="turnright${index}">Turn Right</label></li>
-            <li><input type="checkbox" id="turnleft${index}" onclick="ctrl.applyTransform(this,'turnleft',${index})"/><label for="turnleft${index}">Turn Left</label></li>
-          </ul>
-        </td>
-        <td>
-          <label for="showTile${index}">Tile: </label>
-          <br/>
-          <div id="showTile${index}"></div>
-        </td>
-      </tr>
-    </table>
-  </div>
-</li>`;
-  }
   registry.apply("RogueController",[
     "FileParser",
     "Selector"
@@ -59,7 +31,7 @@
       var ui = {};
       var state = {tiles:{},palettes:{},map:{}};
       var updateView = function() {
-        
+
       }
       this.init = function() {
         Object.entries(domIds).forEach(function(entry){
@@ -102,19 +74,19 @@
       this.drawMap = function(input,outputId) {
 
       }
-      this.updateMapCharIndex = function(input,charListingId) {
+      this.updateMapCharIndex = function(input,charselectId) {
 
       }
-      this.toggleChar = function(input,index) {
+      this.toggleChar = function(input) {
 
       }
-      this.selectTileForChar = function(input,index) {
+      this.selectTileForChar = function(input) {
 
       }
-      this.selectPaletteForChar = function(input,index) {
+      this.selectPaletteForChar = function(input) {
 
       }
-      this.applyTransform = function(index,transform,index) {
+      this.applyTransform = function(index,transform) {
 
       }
     }
