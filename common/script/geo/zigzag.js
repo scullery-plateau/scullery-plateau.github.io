67edb3506774053026f8bc5b9e159a7dfd6ac4f3
Daniel Johnson <daniel.johnson@surescripts.com>
2019-04-12 13:38:57 -0500

    next updates to zigzag

--- common/script/geo/zigzag.js
+++ common/script/geo/zigzag.js
@@ -6,28 +6,75 @@
 </svg>
     `;
   }
+  var frame = function(width,height,grid) {
+    return `<svg width="${width*10}" height="${height*10}">${grid}</svg>`;
+  }
+  var block = function(x,y,isFilled) {
+    return `<rect x="${x * 10}" y="${y * 10}" width="10" height="10" fill="${isFilled?'black':'white'}" stroke="black" stroke-width="1"/>`;
+  }
+  var coord = function(x,y) {
+    return x + "x" + y;
+  }
+  var clearAdjacent = function(points,point,adjacent) {
+    Object.keys(adjacent).forEach(function(key) {
+      if (points[key]) {
+        delete points[key][point];
+      }
+    })
+  }
   var makePoints = function(width,height,count) {
-    var points = [];
-    points.push([Math.floor(Math.random() * width),
-      Math.floor(Math.random() * height)]);
-    var toggle = false;
-    while (points.length < (count - 1)) {
-      var lastY = points[points.length-1][1];
-      var nextX = Math.floor(Math.random() * width);
-      var nextY = Math.floor(Math.random() * height);
-      points.push([nextX,lastY]);
-      points.push([nextX,nextY]);
+    var points = {};
+    for (var x = 0; x < width; x++) {
+      for (var y = 0; y < height; y++) {
+        var adjacent = {};
+        if(x>=1) {
+          adjacent[coord(x-1,y)] = true;
+        }
+        if(x<width-1) {
+          adjacent[coord(x+1,y)] = true;
+        }
+        if(y>=1) {
+          adjacent[coord(x,y-1)] = true;
+        }
+        if(y<height-1) {
+          adjacent[coord(x,y+1)] = true;
+        }
+        points[coord(x,y)] = adjacent;
+      }
     }
-    points.pop();
-    points.push([points[points.length-1][0],points[0][1]]);
-    return points;
+    console.log(points);
+    var per = count * points.length / 100;
+    var blocks = {};
+    var shiftNext = function(availableKeys) {
+      var index = Math.floor(Math.random() * availableKeys.length);
+      var point = points[index];
+      delete points[index];
+      blocks[index] = point;
+      clearAdjacent(points,index,point);
+      clearAdjacent(blocks,index,point);
+    }
+    shiftNext(Object.keys(points));
+    while (Object.keys(points).length < per) {
+      var availableKeys = [].concat.apply([],Object.values(blocks).map(function(block){
+        return Object.keys(block);
+      }));
+      shiftNext(availableKeys);
+    }
+    var addBlock = function(isFilled) {
+      return function(point) {
+        var xy = point.split("x");
+        return block(xy[0],xy[1],isFilled);
+      }
+    }
+    return [].concat(Object.keys(points).map(addBlock(false)),
+      Object.keys(blocks).map(addBlock(true))).join("");
   }
   registry.apply("ZigZag",[
   ],function(){
     return function(width,height,count,fill) {
-      var points = makePoints(width,height,count);
+      var grid = makePoints(width,height,count);
       this.draw = function(canvasId) {
-        document.getElementById(canvasId).innerHTML = template(width,height,fill,points);
+        document.getElementById(canvasId).innerHTML = frame(width,height,grid);
       }
     }
   })
