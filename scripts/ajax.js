ff9291dba3907cc763893e578b3f2f098a585f89
voltron42 <dajohnson1978@gmail.com>
2022-06-10 13:27:02 -0500

    debugging outfitter ajax

--- scripts/ajax.js
+++ scripts/ajax.js
@@ -1,26 +1,50 @@
 (function () {
-  window.getLocalStaticFileAsText = function (filepath, onSuccess, onFailure) {
-    onFailure =
-      onFailure ||
-      ((resp) => {
-        document.dispatchEvent(new CustomEvent('BadAJAX'), { detail: resp });
-      });
-    const xhttp = new XMLHttpRequest();
+  let callbackEvents = {
+    success: 'AjaxSuccess',
+    failure: 'AjaxFailure',
+    stateChange: 'AjaxStateChanged',
+  };
+  let defaultCallbacks = Object.entries(callbackEvents).reduce(
+    (out, [k, v]) => {
+      out[k] = (arg) => {
+        document.dispatchEvent(new CustomEvent(v, { detail: arg }));
+      };
+      return out;
+    },
+    {}
+  );
+  window.getLocalStaticFileAsText = function (filepath, callbacks) {
+    if (typeof callbacks == 'function') {
+      callbacks = {
+        success: callbacks,
+      };
+    }
+    callbacks = Object.entries(callbacks).reduce((out, [k, v]) => {
+      out[k] = v;
+      return out;
+    }, defaultCallbacks);
+    let xhttp = new XMLHttpRequest();
+    xhttp.open('GET', filepath, true);
     xhttp.onreadystatechange = function () {
       if (this.readyState == 4) {
         if (this.status == 200) {
-          onSuccess(this.responseText);
+          callbacks.success(this.responseText);
         } else {
-          onFailure({
+          callbacks.failure({
             requestedFile: filepath,
             status: this.status,
             statusText: this.statusText,
             responseText: this.responseText,
           });
         }
+      } else {
+        callbacks.stateChange({
+          state: this.readyState,
+          min: 0,
+          max: 4,
+        });
       }
     };
-    xhttp.open('GET', filepath,);
     xhttp.send();
   };
 })();
