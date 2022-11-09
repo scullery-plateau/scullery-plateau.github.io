8327cd1b77e4d2e19f871f7e6b2c01ac6da0f98f
voltron42 <voltron42@users.noreply.github.com>
2022-11-09 14:59:04 -0600

    1 file modified

--- react-poc/tokenizer/scripts/tokenizer.js
+++ react-poc/tokenizer/scripts/tokenizer.js
@@ -1,8 +1,36 @@
 namespace('Tokenizer', ['Header'], ({ Header }) => {
+  const [state, setState] = React.useState({ size: 1, tokens: [] });
+  const loadImage = function () {
+    const { size } = state;
+    LoadFile(
+      true,
+      'dataURL',
+      (dataURL, filename) => {
+        setState({
+          size,
+          tokens: [].concat(state.tokens, [{ filename, dataURL, count: 1 }]),
+        });
+      },
+      (filename, error) => {
+        console.log({ filename, error });
+        alert(filename + ' failed to load. See console for error.');
+      }
+    );
+  };
   return function (props) {
     return (
       <>
         <Header menuItems={[]} appTitle={'Tokenizer'} />
+        <div className="d-flex justify-content-center">
+          <button
+            className="btn btn-success"
+            onClick={() => {
+              loadImage();
+            }}
+          >
+            Add Image To Tokenizer
+          </button>
+        </div>
       </>
     );
   };
