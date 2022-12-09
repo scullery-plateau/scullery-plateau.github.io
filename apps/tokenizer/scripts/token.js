017accce5cb25a989f2d28d6571bf4fa7025e35f
Daniel Johnson <voltron42@users.noreply.github.com>
2022-12-09 20:30:39 +0000

    converting Token to component class

--- apps/tokenizer/scripts/token.js
+++ apps/tokenizer/scripts/token.js
@@ -41,29 +41,38 @@ namespace("sp.tokenizer.Token",{"sp.common.Constants":"c"},({c}) => {
   const drawCanvas = function(url,point,setter) {
 
   }
-  const Token = function(props) {
-    const { url, xOffset, yOffset, scale, sideCount, frameColor, backgroundColor, isTransparent, filename } = props.token;
-    const [x, y] = [xOffset, yOffset].map((offset) => ((cxy * (1 - scale)) + offset));
-    const dim = scale * baseScale;
-    const patternId = `pattern-${props.index}`;
-    const [shape, points] = drawShape(sideCount,patternId,frameColor,backgroundColor, isTransparent);
-    const [canvasURL, setCanvasURL] = React.useState("");
-    drawCanvas(url,points,setCanvasURL);
-    console.log({ props, dim, });
-    return <a href={canvasURL.length>0?canvasURL:"#"} download={canvasURL.length>0?`token-${filename}`:''} onClick={ (e) => {
-      if (canvasURL.length > 0) {
-        e.preventDefault();
+  const Token = class extends React.Component {
+    constructor(props) {
+      super(props);
+      this.props = props;
+      this.state = {canvasURL:""};
+    }
+    render() {
+      const { index, token } = this.props;
+      const { url, xOffset, yOffset, scale, sideCount, frameColor, backgroundColor, isTransparent, filename } = token;
+      const [x, y] = [xOffset, yOffset].map((offset) => ((cxy * (1 - scale)) + offset));
+      const dim = scale * baseScale;
+      const patternId = `pattern-${index}`;
+      const [shape, points] = drawShape(sideCount,patternId,frameColor,backgroundColor, isTransparent);
+      drawCanvas(url,points,((canvasUrl) => { this.setState({ canvasURL }); }));
+      console.log({ index, token, dim, });
+      if (url) {
+        return <a href={this.state.canvasURL.length>0?this.state.canvasURL:"#"} download={this.state.canvasURL.length>0?`token-${filename}`:''} onClick={ (e) => {
+          if (this.state.canvasURL.length > 0) {
+            e.preventDefault();
+          }
+        }}>
+          <svg width="100%" height="100%" viewBox={`0 0 ${baseScale} ${baseScale}`}>
+            <defs>
+              <pattern id={ patternId } x="0" y="0" width="100%" height="100%">
+                <image x={x} y={y} width={dim} height={dim} href={url}/>
+              </pattern>
+            </defs>
+            { shape }
+          </svg>
+        </a>;
       }
-    }}>
-      <svg width="100%" height="100%" viewBox={`0 0 ${baseScale} ${baseScale}`}>
-        <defs>
-          <pattern id={ patternId } x={x} y={y} width={`${scale * 100}%`} height={`${scale * 100}%`}>
-            <image x="0" y="0" width={baseScale} height={baseScale} href={url}/>
-          </pattern>
-        </defs>
-        { shape }
-      </svg>
-    </a>;
+    }
   }
   Token.buildInitState = buildInitState;
   return Token;
