d3c1cd00d8fe7b5a19db86a0c39071c58d87c94e
Daniel Johnson <voltron42@users.noreply.github.com>
2023-02-01 16:11:01 +0000

    updating stubs for svengali

---
+++ apps/svengali/scripts/svengali.js
@@ -0,0 +1,8 @@
+namespace('sp.svengali.Svengali',{},() => {
+    const sizeUnits = "mm";
+    const cardSize = { min: 64, max: 89 };
+    const pageSize = { min: 216, max: 279 };
+    return function(props) {
+        return <></>;
+    }
+});
