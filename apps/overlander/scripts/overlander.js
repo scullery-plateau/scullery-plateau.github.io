5a0e1a38dd74f09761578c16cd822bedfbea8ce6
Daniel Johnson <voltron42@users.noreply.github.com>
2023-02-15 22:54:33 +0000

    beginning work on overlander

--- apps/overlander/scripts/overlander.js
+++ apps/overlander/scripts/overlander.js
@@ -6,7 +6,7 @@ namespace('sp.overlander.Overlander',{
     'sp.common.Header':'Header',
     'sp.common.LoadFile':'LoadFile',
     'sp.common.Utilities':'util',
-},({ buildAbout, ColorPicker, Dialog, FileDownload, Header, LoadFile, util, ImageDownload, MondrianSVG }) => {
+},({ buildAbout, ColorPicker, Dialog, FileDownload, Header, LoadFile, util }) => {
     const about = [];
     const validateLoadFileJson = function() {};
     return class extends React.Component {
@@ -15,7 +15,7 @@ namespace('sp.overlander.Overlander',{
             this.state = {};
             this.modals = Dialog.factory({
                 about: {
-                  templateClass: buildAbout("Mondrian",about),
+                  templateClass: buildAbout("Overlander",about),
                   attrs: { class: 'rpg-box text-light w-75' },
                   onClose: () => {}
                 },
@@ -32,10 +32,17 @@ namespace('sp.overlander.Overlander',{
                   },
                 },
             });
+            this.menuItems = [];
         }
         render() {
             return <>
-                <Header menuItems={this.menuItems} appTitle={'Mondrian'} />
+                <Header menuItems={this.menuItems} appTitle={'Overlander'} />
+                <div className="d-flex justify-content-center rpg-box text-light m-1 ">
+                  <svg className="w-75" width="100%" height="100%" viewBox="0 0 800 1000">
+                    <rect width="800" height="1000" fill="white" stroke="black" strokeWidth="4"/>
+                    
+                  </svg>
+                </div>
             </>;
         }
     }
