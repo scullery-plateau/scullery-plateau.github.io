9ea6ce4656ce43dc4b3ebe3bc0686db3262a04ab
Daniel Johnson <voltron42@users.noreply.github.com>
2023-04-14 20:48:17 +0000

    adding layout

--- apps/svengali/scripts/svengali.js
+++ apps/svengali/scripts/svengali.js
@@ -17,6 +17,23 @@ namespace('sp.svengali.Svengali',{
         loadDatafile() {
 
         }
+        buildSpecField(label, fieldName, opts){
+            return util.buildNumberInputGroup(fieldName, label, opts, () => {
+                return this.state.spec[fieldName];
+            }, (value) => {
+                this.updateSpec(fieldName,parseFloat(value));
+            });
+        }
+        buildColorPickerButton(label, fieldName) {
+            return <button 
+                className="btn"
+                style={{
+                }}
+                onClick={() => {}}
+                onDoubleClick={() => {}}
+                onContextMenu={() => {}}
+                >{label}</button>
+        }
         render() {
             return <>
                 { !this.state.datatable &&
@@ -29,11 +46,60 @@ namespace('sp.svengali.Svengali',{
                   </div> }
                 { this.state.datatable &&
                   <div className="rpg-box text-light m-1 d-flex justify-content-center">
-                      <div className="d-flex flex-column">
-
-                      </div>
-                      <div className="d-flex flex-column">
-
+                    <div className="d-flex flex-column">
+                        <div className="d-flex">
+                            <select>
+                                { this.state.layers.map((layer,index) => {
+                                    return <option value={index}>{this.layerLabel(layer)}</option>
+                                }) }
+                            </select>
+                        </div>
+                        <div className="d-flex">
+                            <select>
+                                <option value="text">Text</option>
+                                <option value="image">Image</option>
+                            </select>
+                        </div>
+                        <div className="d-flex">
+                            <textarea></textarea>
+                        </div>
+                        <div className="d-flex">
+                            <select>
+                                { Object.keys(this.state.columns).map((column) => {
+                                    return <option value={column}>{column}</option>;
+                                }) }
+                            </select>
+                        </div>
+                        <div className="d-flex">
+                            { this.buildSpecField("X","x",{}) }
+                            { this.buildSpecField("Y","y",{}) }
+                        </div>
+                        <div className="d-flex">
+                            { this.buildSpecField("Width","width",{}) }
+                            { this.buildSpecField("Height","height",{}) }
+                        </div>
+                        <div className="d-flex">
+                            { this.buildSpecField("RX","rx",{}) }
+                            { this.buildSpecField("RY","ry",{}) }
+                        </div>
+                        <div className="d-flex">
+                            { this.buildColorPickerButton("Fill","fill") }
+                            { this.buildColorPickerButton("Line","line") }
+                        </div>
+                        <div className="d-flex">
+                            { this.buildSpecField("Line Width","strokeWidth",{}) }
+                        </div>
+                    </div>
+                    <div className="d-flex flex-column">
+                        <div className="d-flex">
+                            <select>
+                                { this.state.datatable.map((row) => {
+                                    return <option value={row[this.state.rowLabel]}>{row[this.state.rowLabel]}</option>
+                                }) }
+                            </select>
+                        </div>
+                        <div className="d-flex">
+                        </div>
                       </div>
                   </div> }
             </>;
