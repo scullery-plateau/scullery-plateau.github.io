76421146b977673e84194455be306c13a6bf0675
Daniel Johnson <voltron42@users.noreply.github.com>
2023-11-15 17:33:53 +0000

    layout finalized

--- apps/spritely/scripts/spritely.js
+++ apps/spritely/scripts/spritely.js
@@ -232,25 +232,30 @@ namespace('sp.spritely.Spritely',{
         <>
           <Header menuItems={this.menuItems} appTitle={'Spritely'} />
           <h4 className="text-center">Click <a href="./gallery.html">here</a> view a gallery of Spritely images with datafiles!</h4>
-          <div class="row">
-            <div class="col-2">
-              <div className="d-flex justify-content-around rpg-box m-3 row">
-                <button title="Load File" className="btn btn-primary text-light" onClick={() => {}}><i className="far fa-folder-open"></i></button>
-                <button title="Download" className="btn btn-primary text-light" onClick={() => {}}><i className="far fa-floppy-disk"></i></button>
-                <button title="About" className="btn btn-primary text-light" onClick={() => {}}><i className="far fa-circle-question"></i></button>
+          <div class="row justify-content-center">
+            <div class="col-3">
+              <div className="rpg-box m-3">
+                <div className="d-flex justify-content-around">
+                  <button title="Load File" className="btn btn-primary text-light" onClick={() => {}}><i className="far fa-folder-open"></i></button>
+                  <button title="Download" className="btn btn-primary text-light" onClick={() => {}}><i className="far fa-floppy-disk"></i></button>
+                  <button title="About" className="btn btn-primary text-light" onClick={() => {}}><i className="far fa-circle-question"></i></button>
+                </div>
               </div>
-              <div className="justify-content-center rpg-box m-3 row">
-                { 
-                  transformLabels.map(([id, label, faClass]) => {
-                    return <div className="col-6 justify-content-center m-1">
-                      <button title={label} className="btn btn-primary text-light" onClick={() => {
-                        this.transform(id);
-                      }}><i className={`fas ${faClass}`}></i></button>
-                    </div>;
-                  })
-                }
+              <div className="d-flex flex-column justify-content-center rpg-box m-3">
+                <div className="d-flex justify-content-around my-1">
+                  <button title="Turn Left" className="btn btn-primary text-light" onClick={() => { this.transform("turnLeft") }}><i className={`fas fa-rotate-left`}></i></button>
+                  <button title="Turn Right" className="btn btn-primary text-light" onClick={() => { this.transform("turnRight") }}><i className={`fas fa-rotate-right`}></i></button>
+                  <button title="Flip Over" className="btn btn-primary text-light" onClick={() => { this.transform("flipOver") }}><i className={`fas fa-right-left`}></i></button>
+                  <button title="Flip Down" className="btn btn-primary text-light" onClick={() => { this.transform("flipDown") }}><i className={`fas fa-right-left fa-rotate-90`}></i></button>
+                </div>
+                <div className="d-flex justify-content-around my-1">
+                  <button title="Shift Left" className="btn btn-primary text-light" onClick={() => { this.transform("shiftLeft") }}><i className={`fas fa-arrow-left`}></i></button>
+                  <button title="Shift Right" className="btn btn-primary text-light" onClick={() => { this.transform("shiftRight") }}><i className={`fas fa-arrow-right`}></i></button>
+                  <button title="Shift Up" className="btn btn-primary text-light" onClick={() => { this.transform("shiftUp") }}><i className={`fas fa-arrow-up`}></i></button>
+                  <button title="Shift Down" className="btn btn-primary text-light" onClick={() => { this.transform("shiftDown") }}><i className={`fas fa-arrow-down`}></i></button>
+                </div>
               </div>
-              <div className="d-flex flex-column rpg-box m-3">
+              <div className="d-flex justify-content-around rpg-box m-3">
                 <button className="rounded m-1 btn btn-outline-light"
                         style={{ backgroundColor: this.state.bgColor, color: Colors.getForegroundColor(this.state.bgColor) }}
                         onClick={() => { this.modals.bgColorPicker.open({ color: this.state.bgColor }) }}>BG&nbsp;Color</button>
@@ -259,10 +264,8 @@ namespace('sp.spritely.Spritely',{
                   {this.state.isTransparent ? 'Transparent' : 'Opaque'}
                 </button>
               </div>
-            </div>
-            <div class="col-2">
               <div className="d-flex flex-column rpg-box m-3">
-                <div className="row">
+                <div className="d-flex justify-content-around">
                   <button className="btn btn-success m-1 w-100" title="Add Color"
                       onClick={() => {
                         const selectedPaletteIndex = this.state.palette.length;
@@ -286,7 +289,7 @@ namespace('sp.spritely.Spritely',{
                       this.setState({ palette, selectedPaletteIndex, pixels });
                     }}>-</button>
                 </div>
-                <div className="ml-2 w-100 d-flex flex-wrap">
+                <div className="w-100 d-flex flex-wrap justify-content-center">
                   {this.state.palette.map((color, index) => {
                     const id = SpritelyUtil.getPaletteButtonId(index);
                     return (
@@ -316,7 +319,7 @@ namespace('sp.spritely.Spritely',{
                 </div>
               </div>
             </div>
-            <div class="col-8">
+            <div class="col-7">
               <div className="rpg-title-box m-3" title="click to paint a pixel">
                 <svg width="100%" height="100%" preserveAspectRatio="xMidYMin meet"
                      viewBox={`0 0 ${this.state.size * Constants.pixelDim()} ${this.state.size * Constants.pixelDim()}`}>
