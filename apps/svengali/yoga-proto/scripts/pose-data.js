8ec14eb11130381c65601e200ef2cbca1fbb84bc
Voltron42 <dajohnson1978@gmail.com>
2023-12-26 09:15:28 -0600

    yoga proto functional

--- apps/svengali/yoga-proto/scripts/pose-data.js
+++ apps/svengali/yoga-proto/scripts/pose-data.js
@@ -1,15 +1,54 @@
 namespace("sp.yoga-proto.PoseData",{},() => {
   const categories = {
-    "warm-up":{
-      "rows":[0],
-      "names":["Cat","Cow","Melting Heart"]
+    "Warm-Up":{
+      "firstRow":0,
+      "maxRowWidth": 5,
+      "names":[
+        "Cat", "Cow", "Melting Heart", "Child's", "Standing Forward Bend"
+      ]
     },
-    "stretching":{},
-    "balance":{},
-    "strength":{},
-    "core":{},
-    "backbending":{},
-    "restorative/warm-down":{}
+    "Stretching":{
+      "firstRow":1,
+      "maxRowWidth": 5,
+      "names":[
+        "Pyramid", "Downward Facing Dog", "Garland", "Seated Forward Bend", "Bound Angle", "Head To Knee Forward Bend", "Half Lord Of The Fishes", "Hero", "Wide-Angle Seated Forward Bend"
+      ]
+    },
+    "Balance":{
+      "firstRow":3,
+      "maxRowWidth": 5,
+      "names":[
+        "Tree", "Lord of the Dance", "Eagle", "Standing Split", "Half Moon", "Warrior 3", "Handstand", "Crow", "Extended Hand-to-Big-Tow", "Dancing Shiva"
+      ]
+    },
+    "Strength":{
+      "firstRow":5,
+      "maxRowWidth": 5,
+      "names":[
+        "Warrior 1", "Warrior 2", "Goddess", "Humble Warrior", "Four-Limbed Staff", "Upward Plank", "Chair", "Dolphin", "Triangle", "High Lunge"
+      ]
+    },
+    "Core":{
+      "firstRow":7,
+      "maxRowWidth": 5,
+      "names":[
+        "Plank", "Side Plank", "Boat", "One-Legged Downward Dog", "Scale"
+      ]
+    },
+    "Backbending":{
+      "firstRow":8,
+      "maxRowWidth": 4,
+      "names":[
+        "Cobra", "Upward Facing Dog", "Locust", "Bow", "Camel", "Reclining Hero", "One-Legged King Pigeon", "Upward Bow"
+      ]
+    },
+    "Restorative/Warm-Down":{
+      "firstRow":10,
+      "maxRowWidth": 5,
+      "names":[
+        "Legs Up The Wall", "Shoulder Stand", "Plow", "Easy", "Happy Baby", "Reclining Bound Angle", "Reclining Hand To Big Toe", "Supine Spinal Twist", "Half Pigeon", "Corpse"
+      ]
+    }
   }
   const imgDim = {
     width: 995,
@@ -25,6 +64,7 @@ namespace("sp.yoga-proto.PoseData",{},() => {
     boxWidth: 199,
     rowHeight: 100,
     selectedRow: 0,
-    selectedThumbnail: 0
+    selectedThumbnail: 0,
+    categories
   }
 });
--- apps/svengali/yoga-proto/scripts/yoga-proto.js
+++ apps/svengali/yoga-proto/scripts/yoga-proto.js
@@ -1,5 +1,66 @@
 namespace("sp.yoga-proto.YogaProto",{
-  "sp.yoga-proto.PoseData": "PoseData"
-},({ PoseData }) => {
-  
+  "sp.yoga-proto.PoseData": "PoseData",
+  "sp.common.Utilities":"util",
+},({ PoseData, util }) => {
+  const borderClass = "border border-success border-5"
+  const selectionKey = function(category, name) {
+    return category + ": " + name;
+  }
+  const parseSelectionKey = function(key) {
+    const [category, name] = key.split(": ");
+    return {category, name};
+  }
+  return class extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = util.merge(PoseData, props, { selected:{}, collapsed: {} })
+    }
+    toggleCollapse(e, category) {
+      e.preventDefault();
+      const { collapsed } = this.state;
+      collapsed[category] = !collapsed[category];
+      this.setState({ collapsed });
+    }
+    isSelected(category, name) {
+      return this.state.selected[selectionKey(category,name)];
+    }
+    toggleSelected(e, category, name) {
+      e.preventDefault();
+      const key = selectionKey(category,name);
+      const { selected } = this.state;
+      selected[key] = ! selected[key];
+      this.setState({ selected });
+    }
+    render() {
+      return <>
+        <h2>Yoga Gallery</h2>
+        { Object.entries(this.state.categories).map(([category, { firstRow, maxRowWidth, names }]) => {
+          return <>
+            <div className="card mb-3">
+              <div className="card-header bg-dark text-center">
+                <a className="btn btn-dark" href="#" onClick={(e) => this.toggleCollapse(e, category)}>
+                  <h3>{category}</h3>
+                </a>
+              </div>
+              { !this.state.collapsed[category] && 
+                <div className="card-body bg-info">
+                  <div className="d-flex flex-wrap justify-content-around">
+                    { names.map((name, index) => {
+                      const col = index % maxRowWidth;
+                      const row = firstRow + Math.floor(index / maxRowWidth);
+                      return <div className={`thumbnail m-2 rounded ${this.isSelected(category,name)?borderClass:''}`}>
+                        <a href="#" onClick={(e) => this.toggleSelected(e,category,name)}>
+                          <img width="100%" height="100%" alt={name} title={name} src={`./gallery/pose${col}x${row}.png`}/>
+                        </a>
+                      </div>;
+                    })}
+                  </div>
+                </div>
+              }
+            </div>
+          </>;
+        })}
+      </>;
+    }
+  }
 });
