7d16287149cd9f8ad8d7758a7462b57e84bfda70
Daniel Johnson <voltron42@users.noreply.github.com>
2026-01-25 15:15:09 -0600

    fleshing out svengali

--- apps/svengali/scripts/svengali.js
+++ apps/svengali/scripts/svengali.js
@@ -1,71 +1,98 @@
 namespace('sp.svengali.Svengali',{
-    'sp.common.LoadFile':'LoadFile',
-    'sp.common.Dialog':'Dialog',
-    'sp.common.Header':'Header',
-    'sp.svengali.CardDisplay':'CardDisplay'
+  'sp.common.ColorPicker': 'ColorPicker',
+  'sp.common.Colors': 'Colors',
+  'sp.common.Dialog':'Dialog',
+  'sp.common.EditMode':'EditMode',
+  'sp.common.Header':'Header',
+  'sp.common.LoadFile':'LoadFile',
+  'sp.common.Utilities': 'Utilities',
+
+  'sp.svengali.CardDisplay':'CardDisplay'
 },({ LoadFile, Dialog, Header, CardDisplay }) => {
-    return class extends React.Component {
-        constructor(props){
-            super(props);
-            this.state = {
-              activeTab: 'Layout',
-              layers: [],
-              data: [],
-              schema: []
-            };
-        }
-        setActiveTab(tabName) {
-          this.setState({ activeTab: tabName });
-        }
-        isTabActive(tabName) {
-          return this.state.activeTab == tabName;
-        }
-        buildColorPickerButton(label, fieldName) {
-            return <button 
-                className="btn"
-                style={{
-                }}
-                onClick={() => {}}
-                onDoubleClick={() => {}}
-                onContextMenu={() => {}}
-                >{label}</button>
-        }
-        render() {
-            return <div>
-              <ul className="nav nav-tabs">
-                <li className="nav-item">
-                  <a className={`nav-link${this.isTabActive('Layout')?'active':''}`} href="#" onClick={() => this.setActiveTab('Layout')}>Layout</a>
-                </li>
-                <li className="nav-item">
-                  <a className={`nav-link${this.isTabActive('Data')?'active':''}`} href="#" onClick={() => this.setActiveTab('Data')}>Data</a>
-                </li>
-                <li className="nav-item">
-                  <a className={`nav-link${this.isTabActive('Schema')?'active':''}`} href="#" onClick={() => this.setActiveTab('Schema')}>Schema</a>
-                </li>
-              </ul>
-              <div className="tab-content">
-                { this.isTabActive('Layout') && <div className="row">
-                  <div className="col-5">
-                  </div>
-                  <div className="col-5">
-                    <CardDisplay layers="this.state.layers" data="this.state.data"/>
-                  </div>
-                </div> }
-                { this.isTabActive('Data') && <div className="row">
-                  <div className="col-5">
-                  </div>
-                  <div className="col-5">
-                    <CardDisplay layers="this.state.layers" data="this.state.data"/>
-                  </div>
-                </div> }
-                { this.isTabActive('Schema') && <div className="row">
-                  <div className="col-5">
-                  </div>
-                  <div className="col-5">
-                  </div>
-                </div> }
-              </div>
-            </div>;
-        }
+  Dialog.initializeModals(["alert"], { class: 'rpg-box text-light w-75' });
+  const about = [];
+  return class extends React.Component {
+    constructor(props){
+      super(props);
+      this.state = {
+        activeTab: 'Layout',
+        activeLayer: -1,
+        layers: [],
+        data: [],
+        schema: [],
+      };
+      this.modals = Dialog.factory({
+        colorPicker: {
+          componentClass: ColorPicker,
+          attrs: { class: 'rpg-box text-light w-75' },
+          onClose: ({ color, index }) => {
+            this.setColorFromPicker(index, color);
+          },
+        },
+      });
+      this.menuItems = [
+        {
+          id: 'about',
+          label: 'About',
+          callback: () => {
+            Dialog.alert({ title: "Svengali", lines: about });
+          },
+        },
+      ]
     }
+    setActiveTab(tabName) {
+      this.setState({ activeTab: tabName });
+    }
+    isTabActive(tabName) {
+      return this.state.activeTab == tabName;
+    }
+    buildColorPickerButton(label, fieldName) {
+      return <button 
+        className="btn"
+        style={{
+        }}
+        onClick={() => {}}
+        onDoubleClick={() => {}}
+        onContextMenu={() => {}}
+        >{label}</button>
+    }
+    render() {
+      return <>
+        <Header menuItems={this.menuItems} appTitle={'Svengali'} />
+        <ul className="nav nav-tabs">
+          <li className="nav-item">
+            <a className={`nav-link${this.isTabActive('Layout')?'active':''}`} href="#" onClick={() => this.setActiveTab('Layout')}>Layout</a>
+          </li>
+          <li className="nav-item">
+            <a className={`nav-link${this.isTabActive('Data')?'active':''}`} href="#" onClick={() => this.setActiveTab('Data')}>Data</a>
+          </li>
+          <li className="nav-item">
+            <a className={`nav-link${this.isTabActive('Schema')?'active':''}`} href="#" onClick={() => this.setActiveTab('Schema')}>Schema</a>
+          </li>
+        </ul>
+        <div className="tab-content">
+          { this.isTabActive('Layout') && <div className="row">
+            <div className="col-5">
+            </div>
+            <div className="col-5">
+              <CardDisplay layers="this.state.layers" data="this.state.data"/>
+            </div>
+          </div> }
+          { this.isTabActive('Data') && <div className="row">
+            <div className="col-5">
+            </div>
+            <div className="col-5">
+              <CardDisplay layers="this.state.layers" data="this.state.data"/>
+            </div>
+          </div> }
+          { this.isTabActive('Schema') && <div className="row">
+            <div className="col-5">
+            </div>
+            <div className="col-5">
+            </div>
+          </div> }
+        </div>
+      </>;
+    }
+  }
 });
