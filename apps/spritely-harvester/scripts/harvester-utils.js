cc8f17cb67e1e6450dd2215f30b0db052527461b
Daniel Johnson <voltron42@users.noreply.github.com>
2026-02-08 15:55:35 -0600

    updates to spritely harvester

--- apps/spritely-harvester/scripts/harvester-utils.js
+++ apps/spritely-harvester/scripts/harvester-utils.js
@@ -1,7 +1,80 @@
 namespace("sp.spritely-harvester.HarvesterUtils", {
+  "sp.common.CanvasUtils": "CanvasUtils",
   "sp.common.Colors": "Colors",
-  "sp.common.GridUtilities": "Grid"
-}, ({ Colors, Grid }) => {
+  "sp.common.GridUtilities": "Grid",
+  "sp.common.Utilities": "utils",
+}, ({ CanvasUtils, Colors, Grid, utils }) => {
+  const dims = [ 16, 32, 48 ]
+  const shrinkImage = function(imageUrl, dimSize, callback) {
+    const image = new Image();
+    image.onload = () => {
+      const canvas = document.createElement('canvas');
+      canvas.width = dimSize;
+      canvas.height = dimSize;
+      const context = canvas.getContext('2d');
+      context.drawImage(image, 0, 0);
+      const imageData = context.getImageData(0, 0, dimSize, dimSize);
+      const pixels = imageData.data;
+      const newPixels = {};
+      const newPalette = {};
+      for (let i = 0; i < pixels.length; i += 4) {
+        const pixelIndex = Math.floor(i/4);
+        const col = pixelIndex % canvas.width;
+        const row = Math.floor(pixelIndex/canvas.width);
+        const red = pixels[i];
+        const green = pixels[i + 1];
+        const blue = pixels[i + 2];
+        const hex = Colors.hexFromRgb(red, green, blue);
+        const coord = Grid.getCoordinateId(col, row);
+        newPixels[coord] = hex;
+        newPalette[hex] = (newPalette[hex] || []).concat([coord]);
+      }
+      callback({ newPixels, newPalette });
+    };
+    image.src = imageSrc;
+  };
+  const condensePalette = function(paletteWithCounts) {
+    palette = Object.keys(paletteWithCounts);
+    palette.sort();
+    const distances = {};
+    for (let i = 0; i < palette.length; i++) {
+      for (let j = i + 1; j < palette.length; j++) {
+        const rgbI = Colors.rgbFromHex(palette[i]);
+        const rgbJ = Colors.rgbFromHex(palette[j]);
+        distances[`${palette[i]}x${palette[j]}`] = ["red", "green", "blue"].reduce((sum, color) => sum + Math.pow(rgbI[color] - rgbJ[color], 2), 0);
+      }
+    }
+    const distanceValues = Object.values(distances);
+    distanceValues.sort();
+    const shortest = distanceValues[0];
+    const nearestColors = Object.entries(distances).filter(([_,distance]) => distance == shortest).reduce((acc,[pair]) => {
+      var [i, j] = pair.split("x");
+      acc[i] = (acc[i] || []).concat([pair.replace(acc[i],"").replace("x", "")]);
+      acc[j] = (acc[j] || []).concat([pair.replace(acc[j],"").replace("x", "")]);
+      return acc;
+    }, {});
+    const pairCounts = Object.values(nearestColors).map((pairs) => pairs.length);
+    pairCounts.sort();
+    const highestCount = pairCounts[pairCounts.length - 1];
+    const inverseMap = Object.entries(nearestColors).filter(([_, adjacentColors]) => adjacentColors.length == highestCount).reduce((acc, [color, adjacentColors]) => {
+      return adjacentColors.reduce((outval, adjacentColor) => {
+        outval[adjacentColor] = (outval[adjacentColor] || {});
+        outval[adjacentColor][color] = paletteWithCounts[color];
+        return outval;
+      }, acc);
+    }, {});
+    return Object.entries(inverseMap).reduce((acc, [adjacentColor, targetMap]) => {
+      const targetColors = Object.keys(targetMap);
+      if (targetColors.length == 1) {
+        return targetColors[0];
+      } else {
+        const pixelCounts = Object.values(targetMap);
+        pixelCounts.sort();
+        const maxCount = pixelCounts[pixelCounts.length - 1];
+        return Object.entries(targetMap).filter(([_, pixelCount]) => pixelCount == pixelCounts).map(([targetColor]) => targetColor)[0];
+      }
+    }, {});
+  };
   const extractPixelsFromCanvas = function(imageSrc, callback) {
     const image = new Image();
     image.src = imageSrc;
--- apps/spritely-harvester/scripts/spritely-harvester.js
+++ apps/spritely-harvester/scripts/spritely-harvester.js
@@ -1,12 +1,95 @@
 namespace("sp.spritely-harvester.SpritelyHarvester", {
+  'sp.common.Utilities':'util',
+  'sp.common.LoadFile':'LoadFile',
+  'sp.common.Header': 'Header'
 }, ({}) => {
+  const Thumbnail = function(props) {
+    <div className="thumbnail rpg-box d-flex flex-column">
+      <span className="align-self-center">{props.label}</span>
+      <div
+        className="frame align-self-center"
+        style={{ backgroundImage: `url(${props.dataURL})` }}
+      ></div>
+    </div>
+  }
   return class extends React.Component {
     constructor(props) {
       super(props);
       this.state = {};
+      this.menuItems = [];
+    }
+    loadImage() {
+      LoadFile(
+        false,
+        'dataURL',
+        (dataURL, filename) => {
+          util.initImageObj(dataURL,(baseImg) => {
+            this.setState({ original: { dataURL, baseImg }, filename:filename.split(".")[0] });
+          });
+        },
+        (filename, error) => {
+          console.log({filename, error});
+          alert(filename + ' failed to load. See console for error.');
+        }
+      );
+    }
+    updateDim(updates) {
+      // todo
+    }
+    refinePalette() {
+      // todo
     }
     render() {
-      return <></>;
+      return <>
+        <Header menuItems={this.menuItems} appTitle={'Spritely Harvester'} />
+        { this.state.original ? <>
+          <h1 className="text-center">{this.state.filename}</h1>
+          <div className="d-flex flex-column justify-content-center">
+            <div className="d-flex justify-content-center">
+              <div className="">
+                <label for="coreDim" className="form-label">Select Dimentions from Spritely Defaults:</label>
+                <select id="coreDim" className="form-select" onChange={(e) => {
+                  const dim = e.target.value;
+                  this.updateDim({ rows: dim, columns: dim });
+                }}>
+                  <option value="16">16</option>
+                  <option value="32">32</option>
+                  <option value="48">48</option>
+                </select>
+              </div>
+            </div>
+            <div className="d-flex justify-content-center">
+              <div className="">
+                <label for="rows" className="form-label">Rows</label>
+                <input type="number" id="rows" class="form-control" value={this.state.rows || 0} onChange={(e) => this.updateDim({ rows: e.target.value })}/>
+              </div>
+              <div className="">
+                <label for="columns" class="form-label">Columns</label>
+                <input type="number" id="columns" class="form-control"  value={this.state.columns || 0} onChange={(e) => this.updateDim({ columns: e.target.value })}/>
+              </div>
+            </div>
+            <div className="d-flex justify-content-center">
+              <div className="">
+                <button className="btn btn-primary" onClick={() => this.refinePalette()}>Refine Palette</button>
+              </div>
+            </div>
+            <div className="row justify-content-center">
+              <div className="col-3">
+                <Thumbnail label="Original" dataURL={this.state.original.dataURL}></Thumbnail>
+              </div>
+              { this.state.initial && <div className="col-3">
+                <Thumbnail label="Initial" dataURL={this.state.initial.dataURL}></Thumbnail>
+              </div>}
+              { (this.state.refinements || []).map((refinement, i) => <div className="col-3">
+                <Thumbnail label={`Refinement ${i + 1}`} dataURL={refinement.dataURL}></Thumbnail>
+              </div>)}
+            </div>
+          </div>
+        </> : <>
+          <p>Click the button below to choose an image to convert to a Spritely pixel file.</p>
+          <button className="btn btn-success text-center" onClick={() => this.loadImage()}>Load Image</button>
+        </>}
+      </>;
     }
   }
 });
